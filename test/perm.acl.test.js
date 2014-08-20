e8a1bd92cbc4ebe817233e8740b2d726735f0c3f
david-cahill <davidrichardcahill@gmail.com>
2014-08-20 17:27:40 +0100

    update tests

--- test/perm.acl.test.js
+++ test/perm.acl.test.js
@@ -139,12 +139,24 @@ describe('perm acl', function() {
       hard: true,
       control: 'required',
       actions: ['load', 'save_new', 'save_existing', 'list']
+    },
+    {
+      name: 'hard set to false',
+      roles: ['item_admin'],
+      entities: [{
+        zone: undefined,
+        base: undefined,
+        name: 'list_item'
+      }],
+      hard: false,
+      control: 'required',
+      actions: ['load','save_new', 'save_existing', 'list']
     }],
     allowedProperties: [{
       entity: {
       zone: undefined,
       base: undefined,
-      name: 'email'
+      name: 'list_item'
       },
       fields: ['name', 'number']
     }]
@@ -155,21 +167,41 @@ describe('perm acl', function() {
   })
 
   it('access denied - hard set to true - return permission denied', function(done) {
-    var psi  = si.delegate({perm$:{roles:[]}})
+    var psi  = si.delegate({perm$:{roles:['email_admin']}})
+    var psiList = si.delegate({perm$:{roles:['test_role']}})
 
     var emailItem1 = psi.make('email',{id: 'item1', name: 'Item 1', number: 1, status: 'private'})
-   
-    emailItem1.save$(function(err,emailItem1){
+    var emailItem2 = psiList.make('email')
+
+    ;emailItem1.save$(function(err,emailItem1){
       assert.isNull(err, err)
       assert.isNotNull(emailItem1.id)
       
-    })
-
-    emailItem1.list$(function(err, publicList) {
+    ;emailItem2.list$(function(err, publicList) {
       assert.isNotNull(err, err)
       assert.isNotNull(publicList)
       assert.equal(publicList.length,0)
-    })
+    }) })
+
+    done()
+  })
+
+  it('access denied - hard set to false - return allowed fields only', function(done) {
+    var psi  = si.delegate({perm$:{roles:['item_admin']}})
+    var psiList  = si.delegate({perm$:{roles:['test_role']}})
+
+    var listItem1 = psi.make('list_item',{id: 'item1', name: 'Item 1', number: 1, status: 'private'})
+    var listItem2 = psiList.make('list_item')
+    
+    ;listItem1.save$(function(err,listItem1){
+      assert.isNull(err, err)
+      assert.isNotNull(listItem1.id)
+  
+    ;listItem2.list$(function(err, publicList) {
+      assert.isNotNull(err, err)
+      assert.isNotNull(publicList)
+      assert.equal(publicList.length,0)
+    }) })
 
     done()
   })
